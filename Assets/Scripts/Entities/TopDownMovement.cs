e8f7b486eaa37758950359f247a2694366d5a994
Yejun Kim <yejun283@gmail.com>
2023-09-04 20:35:15 +0900

    Initial Commit

---
+++ Assets/Scripts/Entities/TopDownMovement.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+public class TopDownMovement : MonoBehaviour
+{
+    private TopDownCharacterController _controller;
+
+    private Vector2 _movementDirection = Vector2.zero;
+    private Rigidbody2D _rigidbody;
+
+    Vector2 mouse;
+
+    [SerializeField] private SpriteRenderer characterRenderer;
+
+    private void Awake()
+    {
+        _controller = GetComponent<TopDownCharacterController>();
+        _rigidbody = GetComponent<Rigidbody2D>();
+    }
+
+    private void Start()
+    {
+        _controller.OnMoveEvent += Move;
+    }
+
+    private void FixedUpdate()
+    {
+        ApplyMovment(_movementDirection);
+
+        // Character look at mouse pointer (only x position)
+        mouse = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        characterRenderer.flipX = mouse.x < transform.position.x;
+    }
+
+    private void Move(Vector2 direction)
+    {
+        _movementDirection = direction;
+    }
+
+    private void ApplyMovment(Vector2 direction)
+    {
+        direction = direction * 5;
+
+        _rigidbody.velocity = direction;
+    }
+}
